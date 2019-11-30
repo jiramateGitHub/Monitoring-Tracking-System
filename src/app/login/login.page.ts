6a285bb2cef658bd7c862d783057f0fc31615ff4
Sathien Supabkul <60160353@go.buu.ac.th>
2019-11-30 19:52:12 +0700

    UPLOAD LOGIN

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { url } from 'inspector';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  username;
+  password;
+  Member = [{ username: '60160353', password: '60160353' ,name: 'เสถียร'},
+            { username: '60160157', password: '60160157' ,name: 'จิรเมธ'},
+            { username: '60160104', password: '60160104' ,name: 'เกศรินทร์'},
+            { username: '60160086', password: '60160086' ,name: 'ธนัญญา'},
+            { username: '60160089', password: '60160089' ,name: 'เนตรชนก'},
+            { username: '60160093', password: '60160093' ,name: 'พีรณัฐ'},
+            { username: '60160154', password: '60160154' ,name: 'คณาธิป'},
+            { username: '60160165', password: '60160165' ,name: 'ธัญพิสิษฐ์'},
+            { username: '60160337', password: '60160337' ,name: 'ณัฐธร'},
+            { username: '60160351', password: '60160351' ,name: 'วิภาวี'}]
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  Login(){
+
+    let found = false;
+
+    for(let data of this.Member){
+      if(data.username === this.username && data.password === this.password){
+        found = true;
+        this.router.navigateByUrl('home');
+        break;
+      }                   
+    }
+    if(found === false){
+      alert("Username or Password incorrect");
+    }
   }
 
 }
