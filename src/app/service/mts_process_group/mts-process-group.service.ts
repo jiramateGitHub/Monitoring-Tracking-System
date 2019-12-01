64d29a114e11fc7bedfde6b6a6a16f9154fbe7c0
jiramateGitHub <jiramategithub@gmail.com>
2019-12-01 15:51:12 +0700

    update commit

--- src/app/service/mts_process_group/mts-process-group.service.ts
+++ src/app/service/mts_process_group/mts-process-group.service.ts
@@ -10,11 +10,11 @@ export class MtsProcessGroupService {
   constructor(private http:Http) { }
 
   get_hr_person(){
-    return this.http.get("https://10.80.39.17/TSP60/Thepd-nu/index.php/mts/api/get_hr_person").map(res => res.json());
+    return this.http.get("http://127.0.0.1:3000/hr_person").map(res => res.json());
   }
 
   get_process_group(){
-    return this.http.get("https://10.80.39.17/TSP60/Thepd-nu/index.php/mts/api/get_process_group").map(res => res.json());
+    return this.http.get("http://127.0.0.1:3000/get_process_group").map(res => res.json());
   }
 
   process_group_insert(pcsg_code:string,pcsg_th:string, pcsg_en:string,ps_id:string){
@@ -25,7 +25,6 @@ export class MtsProcessGroupService {
       "ps_id":ps_id,
       "UsPsCode":"60160157"
     }
-    this.http.post("https://10.80.39.17/TSP60/Thepd-nu/index.php/mts/api/process_group_insert",data).map(res => res.text());
-    return this.http.post("https://10.80.39.17/TSP60/Thepd-nu/index.php/mts/api/process_group_insert",data).map(res => res.text());
+    return this.http.post("http://127.0.0.1:3000/process_group_insert",data).map(res => res.text());
   }
 }
